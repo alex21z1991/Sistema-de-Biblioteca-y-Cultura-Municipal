2db7958cbd2df0bd9f9d0776c2ec4cc63900819c
Javier4E <javier.echeverria.soto@alumnos.uta.cl>
2026-09-18 22:12:35 -0300

    typeScript de login creado

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LoginService } from '../../services/login.service';
+import { Router } from '@angular/router';
+import { IUserFormLogin } from '../../interfaces/iuserformlogin';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  loginData: IUserFormLogin{
+    username: '',
+    password: '',
+  }
+
+  errorMessage: string = '';
+
+  constructor(
+    private loginService: LoginService,
+    private router: Router,
+  ){}
+
+  onSubmit(): void {
+    const exito = this.loginService.login(
+      this.loginData
+    );
+  }
 }
--- src/app/interfaces/iuserformlogin.ts
+++ src/app/interfaces/iuserformlogin.ts
@@ -1,5 +1,5 @@
 export interface IUserFormLogin {
     username: string;
     password: string;
-    role: string;
+    role?: string;
 }
